306d251bbb9c2d025b50b78c26eaed5afdee2925
Stepan <stepanchernykh@mail.ru>
2025-04-12 21:46:21 +0800

    тест фронта

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,33 +1,78 @@
 <template>
     <div class="app">
-        <form>
-            <h4>Создание поста</h4>
-            <input class="input" type="text" placeholder="Название">
-            <input class="input" type="text" placeholder="Описание">
-            <button class="btn" @click="createPost">Создать пост</button>
-        </form>
-        <div class="post" v-for="post in posts">
-            <div><strong>Название:</strong>{{ post.title }}</div>
-            <div><strong>Описание:</strong>{{ post.body }}</div>
+        <h1>Страница с постами</h1>
+        <div class="app__btns">
+            <my-button
+                @click="showDialog"
+            >
+                Создать пост
+            </my-button>
+            <my-select
+                v-model="selectedSort"
+                :options="sortOptions"
+            />
         </div>
+        
+        <my-dialog v-model:show="dialogVisible">
+            <post-form
+                @create="createPost"
+            />
+        </my-dialog>
+        <post-list 
+            :posts="posts"
+            @remove="removePost"
+            v-if="!isPostsLoading"
+        />
+        <div v-else>Идет загрузка...</div>
     </div>
 </template>
 
 <script>
+import PostForm from "@/components/PostForm";
+import PostList from "@/components/PostList";
+import axios from "axios";
 export default {
+    components: {
+        PostForm,
+        PostList
+    },
     data() {
         return {
-            posts: [
-                {id: 1, title: 'fffff', body: 'sa;dlfjlskjdf;lks'},
-                {id: 2, title: 'ddd', body: 'sdfgsdfgsdfs'},
-                {id: 3, title: 'ssss', body: 'СsdfgsdfgdsfV'},
-            ],
+            posts: [],
+            dialogVisible: false,
+            isPostsLoading: false,
+            selectedSort: '',
+            sortOptions: [
+                {value: 'title', name: 'По названию'},
+                {value: 'body', name: 'По описанию'},
+            ]
         }
     },
     methods: {
-        createPost() {
-            
-        }
+        createPost(post) {
+            this.posts.push(post); 
+            this.dialogVisible = false;
+        },
+        removePost(post) {
+            this.posts = this.posts.filter(p => p.id !== post.id)
+        },
+        showDialog() {
+            this.dialogVisible = true;
+        },
+        async fetchPosts() {
+            try {
+                this.isPostsLoading = true;
+                const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
+                this.posts = response.data;
+            } catch (e) {
+                alert(e.message);
+            } finally {
+                this.isPostsLoading = false;
+            }
+        },
+    },
+    mounted() {
+        this.fetchPosts();
     }
 }
 </script>
@@ -43,30 +88,9 @@ export default {
     padding: 20px;
 }
 
-.post {
-    padding: 15px;
-    border: 2px solid teal;
-    margin-top: 15px;
-}
-
-form {
+.app__btns {
+    margin: 15px 0;
     display: flex;
-    flex-direction: column;
-}
-
-.input {
-    width: 100%;
-    border: 1px solid teal;
-    padding: 10px 15px;
-    margin-top: 15px;
-}
-
-.btn {
-    margin-top: 15px;
-    align-self: flex-end;
-    padding: 10px 15px;
-    background: none;
-    color: teal;
-    border: 1px solid;
+    justify-content: space-between;
 }
 </style>
---
+++ front/src/components/PostForm.vue
@@ -0,0 +1,51 @@
+<template>
+    <form @submit.prevent>
+        <h4>Создание поста</h4>
+        <my-input 
+            v-model="post.title"
+            placeholder="Введите название поста"
+            style="margin-top: 15px;"
+        />
+        <my-input 
+            v-model="post.body"
+            placeholder="Введите описание поста"
+            style="margin-top: 15px;" 
+        />
+        <my-button 
+            style="align-self: flex-end; margin-top: 15px;"
+            @click="createPost"
+        >
+            Создать пост
+        </my-button>
+    </form> 
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            post: {
+                title: '',
+                body: '',
+            }
+        }
+    },
+    methods: {
+        createPost() {
+            this.post.id = Date.now();
+            this.$emit('create', this.post)
+            this.post = {
+                title: '',
+                body: '',
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+form {
+    display: flex;
+    flex-direction: column;
+}
+</style>
